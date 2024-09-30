1bbf29ca159364af14c3c033eb8bc31c12d252b4
ishimwefrank123 <ishimwefrank147@gmail.com>
2024-09-30 19:47:42 +0200

    create the answers for all the question

---
+++ script.js
@@ -0,0 +1,38 @@
+  const questions = [
+    {
+        question: "Which is larget animal in the world?",
+        answers:[
+            {text: "Shark", correct:false},
+            {text: "Blue whale", correct:true},
+            {text: "Elephant", correct:false},
+            {text: "Giraffe", correct:false}
+        ]
+    },
+    {
+        question: "Which is the smalledst country in the world?",
+        answers:[
+            {text: "Vatoican City", correct:true},
+            {text: "Bhutan", correct:true},
+            {text: "Nepal", correct:false},
+            {text: "Shri Lanka", correct:false}
+        ] 
+    },
+    {
+        question: "Which is the largest  desert in the world?",
+        answers:[
+            {text: "Kalahari", correct:false},
+            {text: "Gobi", correct:false},
+            {text: "Sahara", correct:false},
+            {text: "Antarctica", correct:true}
+        ] 
+    },
+    {
+        question: "Which is the smalledst continet in the world?",
+        answers:[
+            {text: "Asia", correct:false},
+            {text: "Autralia", correct:true},
+            {text: "Arctic", correct:false},
+            {text: "Africa", correct:false}
+        ] 
+    }
+  ];
