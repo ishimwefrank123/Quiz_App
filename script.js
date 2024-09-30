02cb148c8da83b047bee2d37274e9e4ff79431e8
ishimwefrank123 <ishimwefrank147@gmail.com>
2024-09-30 20:03:02 +0200

    create the question using the javascript

--- script.js
+++ script.js
@@ -44,3 +44,24 @@
 
   let currentQuestionIndex = 0;
   let score  = 0;
+
+  function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+  }
+
+  function showQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ".  " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    })
+  }
+  startQuiz();
